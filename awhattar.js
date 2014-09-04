22de3d660ea3d7920865dfa7169df527cc14a5d4
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2014-09-03 23:43:38 -0400

    Remove unused cache variables

--- awhattar.js
+++ awhattar.js
@@ -1,13 +1,9 @@
 "use strict";
 (function(){
 
-var avatarCache = {};
-var canvasCache = {};
-
 function drawOneAvatar(elem) {
 	var canvas = document.createElement("canvas");
 	canvas.width = canvas.height = elem.width;
-	canvasCache[elem.src] = canvas;
 
 	var tempElem = document.createElement("img");
 	tempElem.src = elem.src;
@@ -17,8 +13,6 @@ function drawOneAvatar(elem) {
 	var ctx = canvas.getContext("2d");
 	// draw the middle pixels
 	ctx.drawImage(elem, realWidth / 2, 0, 1, realHeight, 0, 0, canvas.width, canvas.height);
-	//ctx.fillStyle = "blue";
-	//ctx.fillRect(0, 0, elem.width, elem.height);
 	return canvas;
 }
 
